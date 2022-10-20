abf179fa82a979e7acfac0a7f52938e8c2fbf5d6
KasunUmesh <kasunumesh38@gmail.com>
2022-10-20 16:11:03 +0530

    Create Login Page

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane root;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
 }
