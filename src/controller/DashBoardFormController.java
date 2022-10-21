1df8e2895db176a04b7e821dc3d93ffe1c9646e0
KasunUmesh <kasunumesh38@gmail.com>
2022-10-21 19:41:49 +0530

    Create DashBoard

---
+++ src/controller/DashBoardFormController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.StackPane;
+
+public class DashBoardFormController {
+    public StackPane rootFullPage;
+    public StackPane rootHome;
+
+    public void btnHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnEmployeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnVehicleOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnItemOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnItemStockOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSoldDetailsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogOutOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,11 +1,24 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public AnchorPane root;
 
-    public void btnLoginOnAction(ActionEvent actionEvent) {
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(this.getClass().getResource("../view/DashBoardForm.fxml"));
+        Scene scene = new Scene(parent);
+
+        Stage primaryStage = (Stage) this.root.getScene().getWindow();
+
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
     }
 }
