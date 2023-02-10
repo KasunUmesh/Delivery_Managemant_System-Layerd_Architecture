a780356e4e3606cad961886c76ceccdb5d4a0eb2
KasunUmesh <kasunumesh38@gmail.com>
2023-02-10 13:13:50 +0530

    Created Auto Generate Order ID Function

--- src/controller/ItemOrderFormController.java
+++ src/controller/ItemOrderFormController.java
@@ -1,15 +1,22 @@
 package controller;
 
 import animatefx.animation.ZoomIn;
+import bo.BoFactory;
+import bo.custom.ConfirmOrderBO;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
+import view.tdm.NewOrderItemListTM;
+import view.tdm.OrderItemListTM;
+import view.tdm.OrderTM;
 
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -18,32 +25,38 @@ public class ItemOrderFormController {
     public JFXTextField txtOrderNumber;
     public JFXTextField txtItemName;
     public JFXTextField txtItemQty;
-    public JFXComboBox cmbItemCode;
+    public JFXComboBox<String> cmbItemCode;
     public JFXButton btnViewOrderDetail;
     public JFXButton btnViewAddItem;
     public Label lblDate;
     public Pane pnOrderDetails;
-    public TableView tblOrder;
+    public TableView<OrderTM> tblOrder;
     public TableColumn colOrderNumberOrdertb;
     public TableColumn colOrderDate;
     public TableColumn colOrderItemQty;
     public Pane pnOrderItemList;
-    public TableView tblOrderItemList;
+    public TableView<OrderItemListTM> tblOrderItemList;
     public TableColumn colOrderItemCode;
     public TableColumn colOrderItemName;
     public TableColumn colOrderItemDescription;
     public TableColumn colOrderDetailItemQty;
     public Pane pnAddItemList;
-    public TableView tblItemList;
+    public TableView<NewOrderItemListTM> tblItemList;
     public TableColumn colOrderNumber;
     public TableColumn colItemCode;
     public TableColumn colItemName;
     public TableColumn colItemDescription;
     public TableColumn colItemQty;
+    private final ConfirmOrderBO confirmOrderBO = (ConfirmOrderBO) BoFactory.getBoFactory().getBO(BoFactory.BoTypes.CONFIRM_ORDER);
 
     public void initialize(){
 
         loadDate();
+        txtOrderNumber.setText(generateNewOrderID());
+
+        cmbItemCode.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+
+        });
 
         pnAddItemList.setVisible(true);
         btnViewOrderDetail.setVisible(true);
@@ -56,6 +69,17 @@ public class ItemOrderFormController {
         Date date = new Date();
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         lblDate.setText(format.format(date));
+    }
+
+    public  String generateNewOrderID() {
+        try {
+            return confirmOrderBO.generateNewOrderId();
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "Failed to generate a new order id").show();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public void btnAddOnAction(ActionEvent actionEvent) {
